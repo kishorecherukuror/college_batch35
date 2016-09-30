13cc174fb4eacadce95e1093a390606e1078a378
Nisarg Desai <nisarg.desai@mavs.uta.edu>
2016-09-30 09:15:08 -0700

    new teacher section created

---
+++ db/migrate/20160930160414_create_teachers.rb
@@ -0,0 +1,12 @@
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.integer :age
+      t.text :address
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930025554) do
+ActiveRecord::Schema.define(version: 20160930160414) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20160930025554) do
     t.string   "name"
     t.integer  "age"
     t.text     "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.text     "address"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
